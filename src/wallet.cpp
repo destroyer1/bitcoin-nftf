989ae81fe6ff87841eb1c99c0df0dd3ca4a7c15b
ShadowOfHarbringer <shadowofharbringer@bitcoinforums>
2011-06-25 23:26:01 +0200

    Improvements to AllowFree handling. AllowFree() no longer always returns true.

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -824,8 +824,11 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend, CW
 
                 // Check that enough fee is included
                 int64 nPayFee = nTransactionFee * (1 + (int64)nBytes / 1000);
-                bool fAllowFree = CTransaction::AllowFree(dPriority);
-                int64 nMinFee = wtxNew.GetMinFee(1, fAllowFree);
+                
+                //Bitcoin NFTF Patch - changed by ShadowOfHarbringer START
+                int64 nMinFee = wtxNew.GetMinFee(1, true);
+                //Bitcoin NFTF Patch - changed by ShadowOfHarbringer END
+                
                 if (nFeeRet < max(nPayFee, nMinFee))
                 {
                     nFeeRet = max(nPayFee, nMinFee);
